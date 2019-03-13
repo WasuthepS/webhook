0588c3b3279b74b1b0b601c167a53fbc3c0515f8
WasuthepS <wasuthep.sa@officemate.co.th>
2019-03-13 09:07:29 +0700

    wadev

--- main.py
+++ main.py
@@ -7,32 +7,29 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['GET','POST'])
 def webhook():
     if request.method == 'GET':
-
-        data = request.get_json()
-        API_KEY = data['entry'][0]['messaging'][0]['message'] #Key for Verify 
-        hubverify = 'wadev12345'
-      
-       
+        VERIFY_TOKEN = "6C1FD59D73DEC656428B1C9D9AD81" # Key for Verify Token
+        hubverify = request.args.get('hub.verify_token') # Get Verify Key tokem
+        hubchallenge = request.args.get('hub.challenge') # For return to Facebook must to 'CHALLENGE_ACCEPTED'
+        hubmode = request.args.get('hub.mode') # Mode must to 'subscribe'
         
-        if hubverify == API_KEY : # Check data verify and mode
+        if hubverify == VERIFY_TOKEN and hubmode == "subscribe": # Check data verify and mode
             print('WEBHOOK_VERIFIED')
-            return "Wecome  "+ data['UserName'][0]['User'][0]['Name']  , 200 # Return 'Wecome User''
-
+            return hubchallenge , 200 # Return 'CHALLENGE_ACCEPTED'
         
         else:
             return 'You Wrong Something' , 200
 
     elif request.method == 'POST':
-        
         data = request.get_json()
-        
-        
+
         if data['object'] == "page":
-            print(data['entry'][0]['messaging'][0]['message'])
+            #print(data['entry'][0]['messaging'][0]['message'])
             print(data['entry'])
 
-            return data['UserName'][0]['User'][0]['Name'] +"  "+data['entry'][0]['messaging'][1]['message'] , 200
+        return 'EVENT_RECEIVED' , 200
 
+        if hubverify == "bh32q63v2k" and hubmode == "subscribe": # Check data verify and mode
+            return hubchallenge , 200 # Return 'CHALLENGE_ACCEPTED'
 
     elif request.method == 'GET':
         return 'GET' , 200
