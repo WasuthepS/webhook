7047b3fef1fe288a53050ffcc2d8c9e2d208f43f
WasuthepS <wasuthep.sa@officemate.co.th>
2019-03-15 11:11:09 +0700

    new version 15/3/2019

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from flask import Flask, request, jsonify
 #from flask_sslify import SSLify
 
 app = Flask(__name__)
-#sslify = SSLify(app)
 
+# sslify = SSLify(app,subdomains=True)
+# EAALNyShJXH8BAOo5o88mnzJu43t8TqeBl42qGNOna3Gx1RBhPUGvBcFB6tY6RXYNH7Df68Aj6IK3KMtRw9bBiHkeD5h6X7kAAlAxgFb5fiHbp3Udhx2sY7FET8xfIbz5tiLsFynlhDGz0W30Fz4FQFcuL1KZClwZAZA3U0l4gZDZD
 @app.route('/webhook', methods=['GET','POST'])
 def webhook():
     if request.method == 'GET':
-        VERIFY_TOKEN = "6C1FD59D73DEC656428B1C9D9AD81" # Key for Verify Token
+        VERIFY_TOKEN = "ga75HpoblY9qBtOKo2m8QXauNvBoKQzt" # Key for Verify Token
         hubverify = request.args.get('hub.verify_token') # Get Verify Key tokem
         hubchallenge = request.args.get('hub.challenge') # For return to Facebook must to 'CHALLENGE_ACCEPTED'
         hubmode = request.args.get('hub.mode') # Mode must to 'subscribe'
-        
+
         if hubverify == VERIFY_TOKEN and hubmode == "subscribe": # Check data verify and mode
             print('WEBHOOK_VERIFIED')
             return hubchallenge , 200 # Return 'CHALLENGE_ACCEPTED'
@@ -22,13 +23,14 @@ def webhook():
     elif request.method == 'POST':
         data = request.get_json()
 
-        if data['object'] == ["page","user"]:
+        if data['object'] == "page":
             #print(data['entry'][0]['messaging'][0]['message'])
             print(data['entry'])
 
         return 'EVENT_RECEIVED' , 200
 
-        if hubverify == "bh32q63v2k" and hubmode == "subscribe": # Check data verify and mode
+
+        if hubverify == VERIFY_TOKEN and hubmode == "subscribe": # Check data verify and mode
             return hubchallenge , 200 # Return 'CHALLENGE_ACCEPTED'
 
     elif request.method == 'GET':
@@ -43,4 +45,4 @@ def index():
         return 'Service Not Found', 404
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
